f7b2c9c9ed72db94f7f58f0e352b6b52dbeea8be
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-11-04 13:27:07 +0600

    Create app.py

---
+++ flask-dance-google/fdg-v1/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for, render_template, session
+from flask_dance.contrib.google import make_google_blueprint, google
+from dotenv import load_dotenv
+import os
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# Load environment variables from .env file
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+blueprint = make_google_blueprint(
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    scope=[
+        "https://www.googleapis.com/auth/plus.me",
+        "https://www.googleapis.com/auth/userinfo.email",
+    ]
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route("/")
+def index():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    return render_template("welcome.html", email=resp.json()["email"])
+
+@app.route("/logout")
+def logout():
+    token = blueprint.token["access_token"]
+    resp = google.post(
+        "https://accounts.google.com/o/oauth2/revoke",
+        params={"token": token},
+        headers={"content-type": "application/x-www-form-urlencoded"}
+    )
+    assert resp.ok, resp.text
+    del blueprint.token  # Delete OAuth token from storage
+    session.clear()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
