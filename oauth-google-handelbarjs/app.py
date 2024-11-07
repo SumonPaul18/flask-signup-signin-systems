256ee8d3fb579c3a4ab3dd39a8bc4f9709dd1801
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-11-07 15:17:45 +0600

    Update app.py

--- oauth-google-handelbarjs/app.py
+++ oauth-google-handelbarjs/app.py
@@ -1,39 +1,26 @@
+from flask import Flask, redirect, url_for, session, render_template
+import os
+from datetime import timedelta
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
 
-try:
-    from flask import Flask, redirect, url_for, session,render_template
-    import os
-    from datetime import timedelta
-    from authlib.integrations.flask_client import OAuth
-    import json
-    from dotenv import load_dotenv
-    load_dotenv()
-except Exception as e:
-    print("Some Modules are Missing : {} ".format(e))
-
+# Load environment variables
+load_dotenv()
 
 # App config
 app = Flask(__name__)
 
-# ====================Change me  =======================================
-global client_id
-global client_secret
-
-client_id =os.getenv("GOOGLE_CLIENT_ID"),
-client_secret =os.getenv("GOOGLE_CLIENT_SECRET"),
-
 # Session config
-app.secret_key =os.getenv("SECRET_KEY")
+app.secret_key = os.getenv("SECRET_KEY")
 app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
-# ======================================================================
-
 
 # oAuth Setup
 oauth = OAuth(app)
 google = oauth.register(
     name='google',
-    client_id=client_id,
-    client_secret=client_secret,
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
     access_token_url='https://accounts.google.com/o/oauth2/token',
     access_token_params=None,
     authorize_url='https://accounts.google.com/o/oauth2/auth',
@@ -41,20 +28,18 @@ google = oauth.register(
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
     client_kwargs={'scope': 'email profile'},
-    server_metadata_url= 'https://accounts.google.com/.well-known/openid-configuration'
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration'
 )
 
-
-def isLoggedIN():
+def is_logged_in():
     try:
         user = dict(session).get('profile', None)
         if user:
             return True, user.get("given_name")
         else:
-            return False,{}
+            return False, {}
     except Exception as e:
-        return False,{}
-
+        return False, {}
 
 @app.route('/login')
 def login():
@@ -62,18 +47,15 @@ def login():
     redirect_uri = url_for('authorize', _external=True)
     return google.authorize_redirect(redirect_uri)
 
-
 @app.route('/authorize')
 def authorize():
     google = oauth.create_client('google')  # create the google oauth client
     token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+    resp = google.get('userinfo')  # userinfo contains stuff you specified in the scope
     user_info = resp.json()
-    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
     session['profile'] = user_info
-    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    session.permanent = True  # make the session permanent so it keeps existing after browser gets closed
     return redirect('/')
-
 
 @app.route('/logout')
 def logout():
@@ -81,11 +63,10 @@ def logout():
         session.pop(key)
     return redirect('/')
 
-
 @app.route('/')
 def hello_world():
-    flag,user = isLoggedIN()
+    flag, user = is_logged_in()
     return render_template("Index.html", flag=flag, user=user)
 
-app.run(debug=True)
-
+if __name__ == '__main__':
+    app.run(debug=True)
