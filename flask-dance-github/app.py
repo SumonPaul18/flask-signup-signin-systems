b4a4ea3800c7d2fb979c5e853d4cb56aa2d4097e
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2025-04-22 13:04:06 +0600

    Update app.py

--- flask-dance-github/app.py
+++ flask-dance-github/app.py
@@ -19,4 +19,4 @@ def index():
     return "You are @{login} on GitHub".format(login=resp.json()["login"])
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000 )
+    app.run(debug=False, host="0.0.0.0", port=5000 )
