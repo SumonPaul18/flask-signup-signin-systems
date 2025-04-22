20a8132ddd449c17d9c7e00968fd45a4850c731d
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2025-04-22 13:05:49 +0600

    Update app.py

--- mail-otp-verification/app.py
+++ mail-otp-verification/app.py
@@ -34,4 +34,4 @@ def validate():
         return "<h3>Email varification succesfull</h3>"
     return "<h3>Please Try Again</h3>"
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
